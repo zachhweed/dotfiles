3ac880f6e7318c12d550b7a024481b13ba46390b
Zach Weed <zachweedgl@gmail.com>
2016-12-18 13:33:57 -0500

    Modifying beets config a slight bit and just adding the shell of a script to use for syncing, etc.

---
+++ scripts/media_sync.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Figure out syncing. I want to have rsync running a sync from the laptop to the external drive.
+# Once the music is on the external drive I will want to run a beets import from my normal laptop
+# to sync music from the Library folder to Tagged Music and then copy medis from Tagged Music into
+# a directory on my local machine.
