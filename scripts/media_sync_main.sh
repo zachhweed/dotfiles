25bad9694729c340f347678b9765e673806c6a75
Zach Weed <zachweedgl@gmail.com>
2017-01-14 15:23:59 -0500

    Minor updates to iTerm and beets config. Also adding functions for syncing and tagging

--- scripts/media_sync_main.sh
+++ scripts/media_sync_main.sh
@@ -2,6 +2,16 @@
 
 source ./media_sync.sh
 
-if ! crontab -l | grep -q "beet import" ; then
-  crontab -l | sed -i '1 i\*/10 * * * * beet import "$EXTERNAL_MEDIA_DIR"' | crontab -
-fi
+sync_media() {
+  if [ -z ${REMOTE_SERVER+a} ]; then
+    echo "Set remote server in order to sync"
+  else
+    rsync -vhHE --progress -e "ssh -c blowfish" "$REMOTE_SERVER:$EXTERNAL_MEDIA_DIR" "~/Music/Tagged Library" 2> /tmp/media_sync.log
+  fi
+}
+
+tag_media() {
+  if ! crontab -l | grep -q "beet import" ; then
+    crontab -l | sed -i '1 i\*/10 * * * * beet import "$EXTERNAL_MEDIA_DIR"' | crontab -
+  fi
+}
