e142adcc4da9f0857ccd8ca4a5676114a81255de
Zach Weed <zachweedgl@gmail.com>
2017-05-10 08:30:44 -0400

    Furthering directory structure

---
+++ setup/initial_setup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+pretty_print() {
+  local indication="$1"; shift
+  printf "\n$indication\n" "$@"
+}
+
+source ../setup/bash.sh
+source ../setup/ruby.sh
+source ../setup/homebrew.sh
+source ../setup/python.sh
+source ../setup/vim.sh
